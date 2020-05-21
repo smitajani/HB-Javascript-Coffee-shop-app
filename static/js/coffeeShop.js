b5cb031de267c990e6a6181f10b23b67ecce05c4
Smita Jani <smita.jani@gmail.com>
2020-05-21 04:33:43 +0000

    Complete lab excercise

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -1,6 +1,8 @@
 "use strict";
 
+/* Add items to cart */
 const addItemToCart = (itemName) => {
+
   $('#cart-items').append(`
     <tr>
       <td>${itemName}</td>
@@ -8,11 +10,13 @@ const addItemToCart = (itemName) => {
   `);
 };
 
+/* Reset the cart */
 const resetCart = () => {
   $('#cart-total').html('0.00');
   $('#cart-items').empty();
 };
 
+/* Increment the price */
 const incrementCartTotal = (price) => {
   const cartTotal = $('#cart-total');
 
@@ -35,6 +39,20 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 };
 
 
-//
-// Add your event handlers below.
-//
+//Event Handler code..
+
+$('.add-to-order').on('click', () => {
+    addItemToCart("Coffee");
+    incrementCartTotal(1.50);
+  // This is the body of your callback function -- add more
+  // code here!
+  //
+  // (You'll want to call the functions we've already defined
+  // to manipulate the DOM)
+});
+
+$('#place-order').on('click', () => {
+    incrementCoffeeSold($('#cart-items').children().length);
+    resetCart();
+
+});
